6b8a593f6e25fde8c34256846b7df98b4fda93b1
Kuran <kurankumar@gmail.com>
2019-10-22 11:37:10 -0400

    Added comments to code and README

--- apitest.rb
+++ apitest.rb
@@ -3,10 +3,9 @@
 
 require_relative "./config/environment"
 
-def api_response(url:, limit: 100, offset:)
+def api_response(url:, limit: 100, offset:) # Grabs the API url and asks for arguments of limit and offset to grab our data. Limit default to 100.
   # hash = md5(timestamp + private_key + public_key)
   # API request = url + apikey + timestamp + hash
-
   public_key = CONFIG[:my_key]
   private_key = CONFIG[:secret_key]
   ts = Time.now.to_s
@@ -20,11 +19,11 @@ end
 
 puts "working..."
 
-comic_url = "http://gateway.marvel.com/v1/public/comics?"
+comic_url = "http://gateway.marvel.com/v1/public/comics?" # The url which grabs comic's API data.
 
 counter = 0
 chunk_size = 100
-until counter == 3
+until counter == 3 #Loops however many times you need according to counter to grab all of our API data
   puts counter * chunk_size
   response = api_response(url: comic_url, limit: chunk_size, offset: chunk_size * counter)
 
